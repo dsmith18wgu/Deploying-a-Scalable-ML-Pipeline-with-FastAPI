0878970f9288f7af183ea03e6abca445cee156e2
dean <dsmith18@wgu.edu>
2025-09-02 20:09:07 -0500

    updating imports

--- ml/model.py
+++ ml/model.py
@@ -2,8 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing._encoders import OneHotEncoder
-from sklearn.preprocessing._label import LabelBinarizer
+
 
 
 # Optional: implement hyperparameter tuning.
--- train_model.py
+++ train_model.py
@@ -13,10 +13,10 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = "C:/Users/deanb/D501 Project2/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
