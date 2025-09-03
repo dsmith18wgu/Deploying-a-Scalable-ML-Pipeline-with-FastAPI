fd2a19a568269c9c0f90648134c0f334b3461519
dean <dsmith18@wgu.edu>
2025-09-02 19:57:33 -0500

    Remaining main.py functions

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,8 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing._encoders import OneHotEncoder
+from sklearn.preprocessing._label import LabelBinarizer
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
@@ -75,13 +77,12 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    pickle.dump(model, path)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    model = pickle.load(path)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -120,12 +121,16 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    slice_data = data[data[column_name] == slice_value]
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        slice_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = model.predict(X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
